468a30a3c8958823866d6419423e65ec50c9091c
Stefan Drewitz <stefandrewitzyo@gmail.com>
2025-10-07 12:43:10 +0200

    Test(Player): Implement tests for constructor and render function
    - Implemented a test to verify that the Player constructor does not throw errors.
    - Added a test to ensure that the render function executes without errors.
    
    Closes #4

--- tests/test_game/player_test.cpp
+++ tests/test_game/player_test.cpp
@@ -1,2 +1,27 @@
 #include <gtest/gtest.h>
 #include "Game/Player.h"
+
+
+namespace gp {
+    class PlayerTest : public testing::Test {
+    protected:
+        Player player;
+        sf::RenderWindow window{ sf::VideoMode({1920u, 1080u}), "Basic Features" };
+
+        void SetUp() override {
+            window.clear();
+        };
+
+        void TearDown() override {
+            window.close();
+        };
+    };
+
+    TEST_F(PlayerTest, ConstructorGeneratesPlayer) {
+        ASSERT_NO_THROW(Player());
+    }
+
+    TEST_F(PlayerTest, RenderFunctionExecutesWithoutError) {
+        ASSERT_NO_THROW(player.render(window));
+    }
+};
